9f5004a35ebf42b14f4af0b4b111443372805ed5
Michael Mills <mmills6060@gmail.com>
2025-12-01 13:04:31 -0500

    remove typescript backend

--- packages/backend/src/langchain/index.ts
+++
@@ -1,4 +0,0 @@
-export { createCommerceAgent, convertToBaseMessage, extractMessageContent } from './agent.js'
-export { handleStreamingChat, handleNonStreamingChat } from './handlers/streaming-handler.js'
-export type { StreamingState } from './handlers/streaming-handler.js'
-
